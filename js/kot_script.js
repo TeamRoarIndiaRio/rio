6bf8f08976451f7fc36521143ef322d69770c7a1
VB <VB>
2016-07-19 00:23:56 +0530

    minified kot_script.js

--- js/kot_script.js
+++ js/kot_script.js
@@ -1,16 +1,1 @@
-$( document ).ready(function(){
-	//load header and footer
-	$( "header#header" ).load( "templates/header.html" );
-	$( "footer#footer" ).load( "templates/footer.html" );
-	$(".kot-member").flip({
-		"trigger": "hover"
-
-	});
-	// $(".member-pic").click(function(){
-	// 	$(this).flip();
-	// 	// $(this).find(".member-details").addClass("show");
-	// }, function(){
-	// 	$(this).flip(false);
-	// 	// $(this).find(".member-details").removeClass("show");
-	// });
-})
+$(document).ready(function(){$("header#header").load("templates/header.html"),$("footer#footer").load("templates/footer.html"),$(".kot-member").flip({trigger:"hover"})});
